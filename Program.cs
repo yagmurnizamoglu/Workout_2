66adcb1a986ef0a02349560601bcdcc7438a3212
yagmurnizamoglu <121121393+yagmurnizamoglu@users.noreply.github.com>
2022-12-26 00:37:00 +0300

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,97 @@
+﻿namespace asal.sayi
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            #region Soru 2.
+            //int sayac = 0;
+            //int kontrol = 0;
+            //for (int i = 2; i <= 100; i++)
+            //{
+            //    kontrol = 0;
+            //    for (int j = 2; j <= i / 2; j++)
+            //    {
+            //        if (i % j == 0)
+            //        {
+            //            kontrol++;
+            //            break;
+            //        }
+
+            //    }
+            //    if (kontrol == 0)
+            //    {
+            //        sayac++;
+            //        Console.Write(i + "-");
+            //    }
+            //}
+
+            //Console.WriteLine();
+            //Console.ReadKey();
+
+            #endregion
+
+            #region Soru 3.
+
+
+
+            #endregion
+
+            #region Soru 4.
+
+            //int not1 = 0;
+            //int not2 = 0;
+            //int not3 = 0;
+            //int gecmenotu = 0;
+
+            //Console.WriteLine("1. ara sınav notunu yazınız",not1);
+            //not1 = Convert.ToInt32(Console.ReadLine());
+            //Console.WriteLine("2. ara sınav notunu yazınız", not2);
+            //not2 = Convert.ToInt32(Console.ReadLine());
+            //Console.WriteLine("Genel sınav notunu yazınız", not3);
+            //not3 = Convert.ToInt32(Console.ReadLine());
+            //gecmenotu = Convert.ToInt32(0.2*not1+0.3*not2+0.5*not3);
+            //Console.WriteLine();
+            //Console.WriteLine("Ortalamanız = {0}", gecmenotu);
+            //if (gecmenotu < 50)
+            //    Console.WriteLine("Kaldınız.");
+            //else
+            //    Console.WriteLine("Geçtiniz.");
+
+            //Console.ReadKey();
+
+            #endregion
+
+            #region Soru 5.
+
+            //int sayi, binler, yuzler, onlar, birler, bin, yuz, on, bir, fark;
+
+            //Console.Write("Sayıyı giriniz:");
+            //sayi = Convert.ToInt32(Console.ReadLine());
+
+            //binler = sayi / 1000;
+            //bin = sayi - (binler * 1000);
+
+            //yuzler = bin / 100;
+            //yuz = bin - (yuzler * 100);
+
+            //onlar = yuz / 10;
+            //on = yuz - (onlar * 10);
+
+            //bir = on * 1;
+
+
+            //fark = (yuzler - (yuzler / 3) * 3) - bir;
+
+            //Console.WriteLine("Cevap" + fark);
+
+            //Console.ReadKey();
+
+
+            #endregion
+
+
+
+        }
+    }
+ }
